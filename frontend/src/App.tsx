a3a12639cf20b19f454a7664a9b4d35873869b34
Lucien Brule <lucien@lucienbrule.com>
2023-02-27 00:19:11 -0500

    add health status indicator, it's green

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,51 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [online, isOnline] = useState(false)
+  useEffect(() => {
+
+    const healthCheck = async () => {
+
+      try {
+        const res = await fetch('/api/v1/health')
+        const data = await res.json()
+        console.log(data)
+        if (data["status"] === "ok") {
+          isOnline(true)
+        }
+      } catch (e) {
+        console.error(e)
+        isOnline(false)
+      }
+    }
+
+    const interval = setInterval(() => {
+        healthCheck()
+      }
+      , 1000)
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div className="App">
+
+      {online ? <div className="online status_indicator"/> : <div className="offline status_indicator"/>}
+
+      <div className="card">
+        <h2>Getting Started</h2>
+        <p>
+          Observe the status indicator above. It should be green if the server is running.
+        </p>
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo"/>
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
