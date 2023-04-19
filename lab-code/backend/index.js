8cfee4ec71c997ff6083b0e98c3da68524a9eff6
CianHowell <91489341+CianHowell@users.noreply.github.com>
2023-04-18 20:58:24 -0400

    Update index.js

--- lab-code/backend/index.js
+++ lab-code/backend/index.js
@@ -9,44 +9,44 @@ const app = express();
 const statesMap = new Map(Object.entries(states));
 const countiesMap = new Map(counties.map(county => [county.fips_code, county]));
 
-// async function preloadDocs() {
-//     await client.connect();
-//     const db = client.db("Lab6");
-//     const collection = db.collection("NASA Data");
-//     console.log("Connected to MongoDB");
-//     for (let i = 100; i < 301; i++) {
+async function preloadDocs() {
+    await client.connect();
 
-//         let lat = parseFloat(counties[i].lat);
+    for (let i = 100; i < 101; i++) {
 
-//         let lng = parseFloat(counties[i].lng);
+        let lat = parseFloat(counties[i].lat);
 
-//         let fips_code = counties[i].fips_code;
+        let lng = parseFloat(counties[i].lng);
 
-//         let name_1 = counties[i].name;
+        let fips_code = counties[i].fips_code;
 
-//         let state = parseFIPS(fips_code)[0];
+        let name_1 = counties[i].name;
 
-//         let county = parseFIPS(fips_code)[1];
+        let state = parseFIPS(fips_code)[0];
 
-//         let stateAbbr = states[state];
+        let county = parseFIPS(fips_code)[1];
 
-//         let POWERAPI = "https://power.larc.nasa.gov/api/temporal/climatology/point?parameters=PS,WS10M,WS10M_MAX,WS10M_MIN,WS10M_RANGE,WS50M,WD10M,WS50M_MAX,WS50M_MIN,WS50M_RANGE,WD50M&community=RE&longitude=" + lng + "&latitude=" + lat + "&format=JSON";
+        let stateAbbr = states[state];
 
-//         let censusAPI = "https://api.census.gov/data/2019/acs/acs5?get=NAME,B19013_001E&for=county:" + county + "&in=state:" + state + "&key=" + process.env.CENSUS_API_KEY;
+        let POWERAPI1 = "https://power.larc.nasa.gov/api/temporal/climatology/point?parameters=ALLSKY_SFC_SW_DWN,ALLSKY_SFC_SW_DWN_HR,ALLSKY_SFC_SW_DNI,ALLSKY_SFC_SW_DIFF,ALLSKY_KT,TOA_SW_DWN,CLOUD_AMT,ALLSKY_SFC_PAR_TOT,ALLSKY_SRF_ALB,SI_EF_TILTED_SURFACE,CLRSKY_SFC_SW_DWN&community=RE&longitude=" + lng + "&latitude=" + lat + "&format=JSON";
 
-//         let eiaAPI = "https://api.eia.gov/v2/electricity/state-electricity-profiles/summary/data/?frequency=annual&data[0]=average-retail-price&data[1]=capacity-ipp&data[2]=carbon-dioxide-lbs&data[3]=direct-use&data[4]=generation-elect-utils&facets[stateID][]=" + stateAbbr + "&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=" + process.env.EIA_API_KEY;
+        let POWERAPI2 = "https://power.larc.nasa.gov/api/temporal/climatology/point?parameters=SG_NOON,SG_HR_SET_ANG,SG_MID_COZ_ZEN_ANG,SG_DEC,SG_DAY_HOURS,SG_HRZ_HR,SG_DAY_COZ_ZEN_AVG,T2M,T2MDEW,T2MWET,TS,FROST_DAYS,QV2M,RH2M,PRECTOTCORR,PRECTOTCORR_SUM&community=RE&longitude=" + lng + "&latitude=" + lat + "&format=JSON";
 
-//         let newCollection = await normalizeData(name_1, stateAbbr, POWERAPI, censusAPI, eiaAPI);
-//         await collection.insertOne(newCollection);
-//     }
-// }
+        let POWERAPI3 = "https://power.larc.nasa.gov/api/temporal/climatology/point?parameters=PS,WS10M,WD50M,WD10M,WS50M,CLOUD_AMT,SOLAR_DEFICITS_CONSEC_MONTH,INSOL_CONSEC_MONTH,MIDDAY_INSOL,EQUIV_NO_SUN_CONSEC_MONTH&community=RE&longitude=" + lng + "&latitude=" + lat + "&format=JSON";
 
-// preloadDocs();
+        let censusAPI = "https://api.census.gov/data/2019/acs/acs5?get=NAME,B19013_001E&for=county:" + county + "&in=state:" + state + "&key=" + process.env.CENSUS_API_KEY;
+
+        let eiaAPI = "https://api.eia.gov/v2/electricity/state-electricity-profiles/summary/data/?frequency=annual&data[0]=average-retail-price&data[1]=capacity-ipp&data[2]=carbon-dioxide-lbs&data[3]=direct-use&data[4]=generation-elect-utils&facets[stateID][]=" + stateAbbr + "&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=" + process.env.EIA_API_KEY;
+
+        let newCollection = await normalizeData(name_1, stateAbbr, POWERAPI1, POWERAPI2, POWERAPI3, censusAPI, eiaAPI);
+
+        console.log(newCollection);
+    }
+}
+
+preloadDocs();
 
 async function addData(county, state) {
-    const db = client.db("Lab6");
-    const collection = db.collection("NASA Data");
-    console.log("Connected to MongoDB");
     let stateFips;
     for (const [fips, stateAbbr] of statesMap) {
         if (stateAbbr === state) {
@@ -72,72 +72,76 @@ async function addData(county, state) {
         let lat = countyEntry.lat;
         let lng = countyEntry.lng;
 
-        console.log( countyFips, stateFips, lat, lng);
+        console.log(countyFips, stateFips, lat, lng);
 
-        let POWERAPI = "https://power.larc.nasa.gov/api/temporal/climatology/point?parameters=PS,WS10M,WS10M_MAX,WS10M_MIN,WS10M_RANGE,WS50M,WD10M,WS50M_MAX,WS50M_MIN,WS50M_RANGE,WD50M&community=RE&longitude=" + lng + "&latitude=" + lat + "&format=JSON";
+        let POWERAPI1 = "https://power.larc.nasa.gov/api/temporal/climatology/point?parameters=ALLSKY_SFC_SW_DWN,ALLSKY_SFC_SW_DWN_HR,ALLSKY_SFC_SW_DNI,ALLSKY_SFC_SW_DIFF,ALLSKY_KT,TOA_SW_DWN,ALLSKY_SFC_PAR_TOT,ALLSKY_SRF_ALB,SI_EF_TILTED_SURFACE,CLRSKY_SFC_SW_DWN&community=RE&longitude=" + lng + "&latitude=" + lat + "&format=JSON";
 
+        let POWERAPI2 = "https://power.larc.nasa.gov/api/temporal/climatology/point?parameters=SG_NOON,SG_HR_SET_ANG,SG_MID_COZ_ZEN_ANG,SG_DEC,SG_DAY_HOURS,SG_HRZ_HR,SG_DAY_COZ_ZEN_AVG,T2M,T2MDEW,T2MWET,TS,FROST_DAYS,QV2M,RH2M,PRECTOTCORR,PRECTOTCORR_SUM&community=RE&longitude=" + lng + "&latitude=" + lat + "&format=JSON";
+
+        let POWERAPI3 = "https://power.larc.nasa.gov/api/temporal/climatology/point?parameters=PS,WS10M,WD50M,WD10M,WS50M,CLOUD_AMT,SOLAR_DEFICITS_CONSEC_MONTH,INSOL_CONSEC_MONTH,MIDDAY_INSOL,EQUIV_NO_SUN_CONSEC_MONTH&community=RE&longitude=" + lng + "&latitude=" + lat + "&format=JSON";
         let censusAPI = "https://api.census.gov/data/2019/acs/acs5?get=NAME,B19013_001E&for=county:" + countyFips + "&in=state:" + stateFips + "&key=" + process.env.CENSUS_API_KEY;
 
         let eiaAPI = "https://api.eia.gov/v2/electricity/state-electricity-profiles/summary/data/?frequency=annual&data[0]=average-retail-price&data[1]=capacity-ipp&data[2]=carbon-dioxide-lbs&data[3]=direct-use&data[4]=generation-elect-utils&facets[stateID][]=" + state + "&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=" + process.env.EIA_API_KEY;
 
-        let newCollection = await normalizeData(county, state, POWERAPI, censusAPI, eiaAPI);
-
-        await collection.insertOne(newCollection);
+        let newCollection = await normalizeData(county, state, POWERAPI1, POWERAPI2, POWERAPI3, censusAPI, eiaAPI);
+        console.log(newCollection);
     }
 }
 
-// function parseFIPS(fips_code) {
-//     fips_code = fips_code.toString();
-//     let state;
-//     state = fips_code.substring(0, 2);
-//     let county;
-//     county = fips_code.substring(2, 5);
-//     return [state, county];
-// }
+function parseFIPS(fips_code) {
+    fips_code = fips_code.toString();
+    let state;
+    state = fips_code.substring(0, 2);
+    let county;
+    county = fips_code.substring(2, 5);
+    return [state, county];
+}
 
-async function normalizeData(county, state, POWERAPI, censusAPI, eiaAPI) {
+async function normalizeData(county, state, POWERAPI1, POWERAPI2, POWERAPI3, censusAPI, eiaAPI) {
     let collection = {
         county: county,
         state: state,
-        monthlyData: [],
-        householdIncome: 0,
-        stateStats: []
+        NASA_power_data: {},
+        household_income: 0,
+        EIA_data: [],
+        solar_rank: 0,
+        wind_rank: 0,
+        hydro_rank: 0
     };
 
-    const [powerData, censusData, electricData] = await Promise.all([fetch(POWERAPI), fetch(censusAPI), fetch(eiaAPI)]);
+    const [powerData1, powerData2, powerData3, censusData, electricData] = await Promise.all([fetch(POWERAPI1), fetch(POWERAPI2), fetch(POWERAPI3), fetch(censusAPI), fetch(eiaAPI)]);
 
-    const [powerJson, censusJson, electricJson] = await Promise.all([powerData.json(), censusData.json(), electricData.json()]);
+    const [powerJson1, powerJson2, powerJson3, censusJson, electricJson] = await Promise.all([powerData1.json(), powerData2.json(), powerData3.json(), censusData.json(), electricData.json()]);
 
-    let parameterArray;
-    parameterArray = ["PS", "WD10M", "WD50M", "WS10M", "WS50M", "WS10M_MAX", "WS10M_MIN", "WS50M_MAX", "WS50M_MIN", "WS10M_RANGE", "WS50M_RANGE"];
-    let monthArray;
-    monthArray = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "ANN"];
-    for (let i = 0; i < parameterArray.length; i++) {
-        let parameter = {
-            parameter: parameterArray[i],
-            monthlyData: []
-        };
-        for (let j = 0; j < monthArray.length; j++) {
-            let monthlyData = {
-                month: monthArray[j],
-                value: powerJson.properties.parameter[parameterArray[i]][monthArray[j]]
-            };
-            parameter.monthlyData.push(monthlyData);
-        }
-        collection.monthlyData.push(parameter);
-    }
-    collection.householdIncome = censusJson[1][1];
+
+    let powerParams1 = Object.keys(powerJson1.properties.parameter);;
+    let powerParams2 = Object.keys(powerJson2.properties.parameter);
+    let powerParams3 = Object.keys(powerJson3.properties.parameter);
+
+    let powerParams = powerParams1.concat(powerParams2, powerParams3);
+
+    let powerJson = { ...powerJson1.properties.parameter, ...powerJson2.properties.parameter, ...powerJson3.properties.parameter };
+
+    let powerJsonParams = {...powerJson1.parameters, ...powerJson2.parameters, ...powerJson3.parameters};
+
+    powerParams.map((param) => {
+        collection.NASA_power_data[param] = powerJson[param];
+        collection.NASA_power_data[param].units = powerJsonParams[param].units;
+        collection.NASA_power_data[param].longname = powerJsonParams[param].longname;
+    });
+
+    collection.household_income = censusJson[1][1];
     for (let i = 0; i < electricJson.response.data.length; i++) {
         let stateStats = void 0;
         stateStats = {
-            year: electricJson.response.data[i].period,
-            averageRetailPrice: electricJson.response.data[i]["average-retail-price"],
-            capacityIPP: electricJson.response.data[i]["capacity-ipp"],
-            carbonDioxideLbs: electricJson.response.data[i]["carbon-dioxide-lbs"],
-            directUse: electricJson.response.data[i]["direct-use"],
-            generationElectUtils: electricJson.response.data[i]["generation-elect-utils"]
+            YEAR: electricJson.response.data[i].period,
+            AVERAGE_RETAIL_PRICE: electricJson.response.data[i]["average-retail-price"],
+            CAPACITY_IPP: electricJson.response.data[i]["capacity-ipp"],
+            CARBON_DIOXIDE_LBS: electricJson.response.data[i]["carbon-dioxide-lbs"],
+            DIRECT_USE: electricJson.response.data[i]["direct-use"],
+            GENERATION_ELECT_UTILS: electricJson.response.data[i]["generation-elect-utils"]
         };
-        collection.stateStats.push(stateStats);
+        collection.EIA_data.push(stateStats);
     }
     return collection;
 }
@@ -161,70 +165,15 @@ app.get('/:state/:county', async (req, res) => {
 });
 
 app.post('/:state/:county', async (req, res) => {
-    let state = req.params["state"];
-    let county = req.params["county"];
-    const db = client.db("Lab6");
-    const collection = db.collection("NASA Data");
-    const query = { county: county, state: state };
-    const data = await collection.find(query).toArray();
-    if (data.length == 0) {
-        addData(county, state);
-        json = {
-            success: "Data added"
-        };
-        res.json(json);
-    }
-    else {
-        json = {
-            error: "Data already exists"
-        };
-        res.json(json);
-    }
+    res.send("Data Updated");
 });
 
 app.delete('/:state/:county', async (req, res) => {
-    let state = req.params["state"];
-    let county = req.params["county"];
-    const db = client.db("Lab6");
-    const collection = db.collection("NASA Data");
-    const query = { county: county, state: state };
-    const data = await collection.find(query).toArray();
-    if (data.length == 0) {
-        json = {
-            error: "No data to delete"
-        };
-        res.json(json);
-    }
-    else {
-        const result = await collection.deleteOne(query);
-        json = {
-            success: "Data deleted"
-        };
-        res.json(json);
-    }
+    res.send("Data Updated");
 });
 
 app.put('/:state/:county', async (req, res) => {
-    let state = req.params["state"];
-    let county = req.params["county"];
-    const db = client.db("Lab6");
-    const collection = db.collection("NASA Data");
-    const query = { county: county, state: state };
-    const data = await collection.find(query).toArray();
-    if (data.length == 0) {
-        json = {
-            error: "No data to update"
-        };
-        res.json(json);
-    }
-    else {
-        const result = await collection.deleteOne(query);
-        addData(county, state);
-        json = {
-            success: "Data updated"
-        };
-        res.json(json);
-    }
+    res.send("Data Updated");
 });
 
 app.listen(3000, async () => {
