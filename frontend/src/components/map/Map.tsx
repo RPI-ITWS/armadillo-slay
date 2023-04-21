ef8df23b379ef259873e8a7e36407db09c768f25
CianHowell <91489341+CianHowell@users.noreply.github.com>
2023-04-21 09:32:57 -0400

    I am done for now

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -1,18 +1,28 @@
-import React, { useEffect, useRef } from 'react';
-import ReactMapGL, { Marker, ViewState, NavigationControl } from 'react-map-gl';
+import React, { useEffect, useRef, useState } from 'react';
+import ReactMapGL, { Marker, ViewState, NavigationControl, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiY2lhbmhvd2VsbCIsImEiOiJjbGdxYmkwMDcwYnZkM3JzYm9pM2hpZDdqIn0.2CJzcHp1dfHl5tw-Nl6Lrw';
 
 function Map() {
- 
-  const [viewport, setViewport] = React.useState({
+
+  const [viewport, setViewport] = useState({
     latitude: 42.6737,
     longitude: -73.747253,
     zoom: 9,
     width: '100vw',
     height: '100vh',
   });
+
+  const [popupVisible, setPopupVisible] = useState(true);
+
+  const handleMarkerClick = () => {
+    setPopupVisible(true);
+  };
+
+  const handlePopupClose = () => {
+    setPopupVisible(false);
+  };
 
   return (
     <ReactMapGL
@@ -33,6 +43,28 @@ function Map() {
           />
         </div>
       </Marker>
+      {popupVisible && (
+        <Popup
+          latitude={42.6737}
+          longitude={-73.747253}
+          closeButton={true}
+          closeOnClick={false}
+          onClose={handlePopupClose}
+          anchor="top"
+        >
+          <div>Household Income: $68,991
+            <br />
+            Renewable Energy Ranking:
+            <br />
+            1. Solar
+            <br />
+            2. Wind
+            <br />
+            3. Hydroelectric
+            <br />
+          </div>
+        </Popup>
+      )}
       <NavigationControl
         onViewportChange={(viewport: ViewState) => setViewport(viewport)}
         showCompass={false}
